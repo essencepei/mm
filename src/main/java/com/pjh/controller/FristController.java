fed1cdfd5d058286ff1cbbbdcb4875dd47589126
essencepei <essencepei@gmail.com>
2016-10-11 00:03:23 +0800

    ++++

--- src/main/java/com/pjh/controller/FristController.java
+++ src/main/java/com/pjh/controller/FristController.java
@@ -7,8 +7,10 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 @RequestMapping("/fristController")
 public class FristController {
+	
 	@RequestMapping("/index.do")
 	public ModelAndView page(){
+		System.out.println("????");
 		ModelAndView mav = new ModelAndView();
 		mav.setViewName("index");
 		return mav;
