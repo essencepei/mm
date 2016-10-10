da8e05196cf216b6f36f1d24400d5d67159f2965
essencepei <essencepei@gmail.com>
2016-10-10 23:26:55 +0800

    first commit

---
+++ src/main/java/com/pjh/controller/FristController.java
@@ -0,0 +1,17 @@
+package com.pjh.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/fristController")
+public class FristController {
+	@RequestMapping("/index.do")
+	public ModelAndView page(){
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName("index");
+		return mav;
+		
+	}
+}
