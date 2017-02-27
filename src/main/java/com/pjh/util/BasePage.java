3024498629d5ab06e34ab14b31b6862dde0ca2e5
essencepei <essencepei@gmail.com>
2017-02-27 14:30:19 +0800

    删除basepage做一下同步

--- src/main/java/com/pjh/util/BasePage.java
+++ src/main/java/com/pjh/util/BasePage.java
@@ -1,5 +1,37 @@
 package com.pjh.util;
 
+import java.util.HashMap;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.pjh.dao.CaseInfoMapper;
+import com.pjh.dao.CaseMapper;
+import com.pjh.dao.ElementMapper;
+import com.pjh.dao.PageMapper;
+
 public class BasePage {
+	protected WebDriver driver;
+	@Autowired
+	CaseInfoMapper caseinfo;
+	@Autowired
+	CaseMapper caselist;
+	@Autowired
+	ElementMapper element;
+	@Autowired
+	PageMapper page;
+	
+	public enum ByType {
+		xpath, id, linkText, name, className, cssSelector, partialLinkText, tagName
+	}
+	
+	HashMap<String, Locator> locatorMap;
+	
+	public static void main(String[] args) {
+		WebDriver driver =  null;
+		
+	}
+	
 
 }
