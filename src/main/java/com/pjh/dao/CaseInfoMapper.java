945c56ed31b52fa2797c319e7d2d0796ced393b2
essencepei <essencepei@gmail.com>
2017-02-27 14:29:47 +0800

    通过页面对象查找页面元素

--- src/main/java/com/pjh/dao/CaseInfoMapper.java
+++ src/main/java/com/pjh/dao/CaseInfoMapper.java
@@ -7,5 +7,7 @@ import com.pjh.model.CaseInfo;
 public interface CaseInfoMapper {
 	//查询
 	public List<CaseInfo> querycaseinfo(Map<String,Object> param);
+	//通过页面名称查询页面元素
+	public List<CaseInfo> queryByPageName(String param);
 	
 }
